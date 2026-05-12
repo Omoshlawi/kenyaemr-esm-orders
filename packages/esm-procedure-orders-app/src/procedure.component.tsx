4eec496c8f96d9266deca13cc032a466dcbfbd66
Laurent Ouma <98098891+Omoshlawi@users.noreply.github.com>
2026-05-12 09:20:05 +0300

    feat: add feature flag for procedure queues and update rendering logic (#18)

--- packages/esm-procedure-orders-app/src/procedure.component.tsx
+++ packages/esm-procedure-orders-app/src/procedure.component.tsx
@@ -1,15 +1,16 @@
-import { ExtensionSlot, useDefineAppContext } from '@openmrs/esm-framework';
+import { ExtensionSlot, useDefineAppContext, useFeatureFlag } from '@openmrs/esm-framework';
 import dayjs from 'dayjs';
 import React, { useState } from 'react';
 import { ProcedureHeader } from './header/procedure-header.component';
 
 import { type DateFilterContext } from './types';
 import { useOrdersWorklist } from './hooks/useOrdersWorklist';
+import ProcedureOrdersTabs from './procedures-ordered/procedure-tabs.component';
 
 const Procedure: React.FC = () => {
   const [dateRange, setDateRange] = useState<Date[]>([dayjs().startOf('day').toDate(), new Date()]);
   useDefineAppContext<DateFilterContext>('procedures-date-filter', { dateRange, setDateRange });
-
+  const isprocedureQueueEnabled = useFeatureFlag('procedureQueues');
   // Orders required by the ProcedureQueue component from express workflow
   const { workListEntries: activeOrders } = useOrdersWorklist('', '');
   const { workListEntries: inProgressOrders } = useOrdersWorklist('', 'IN_PROGRESS');
@@ -18,10 +19,14 @@ const Procedure: React.FC = () => {
   return (
     <div className={`omrs-main-content`}>
       <ProcedureHeader />
-      <ExtensionSlot
-        name="procedure-orders-dashboard-slot"
-        state={{ activeOrders, inProgressOrders, completedOrders }}
-      />
+      {isprocedureQueueEnabled ? (
+        <ExtensionSlot
+          name="procedure-orders-dashboard-slot"
+          state={{ activeOrders, inProgressOrders, completedOrders }}
+        />
+      ) : (
+        <ProcedureOrdersTabs />
+      )}
     </div>
   );
 };
